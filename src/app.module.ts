7ab453285cb99100575f20fdd88f3c17cdfbd972
oluwatobi Adeenkan <tadenekan@dlabs.cloud>
2020-01-26 01:57:47 +0100

    chore/auth-service Added nest js core module for congig

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,20 @@ import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {DomainModule} from './domain/domain.module';
 import {CoreModule} from './core/core.module';
-import { DaoModule } from './dao/dao.module';
-import { ControllerModule } from './controller/controller.module';
-import { DtoModule } from './dto/dto.module';
-import { ServiceModule } from './service/service.module';
-import { DLabsCommonModule } from './d-labs-common/d-labs-common.module';
+import {DaoModule} from './dao/dao.module';
+import {ControllerModule} from './controller/controller.module';
+import {DtoModule} from './dto/dto.module';
+import {ServiceModule} from './service/service.module';
+import {DLabsCommonModule} from './d-labs-common/d-labs-common.module';
+import {ConfigModule} from '@nestjs/config';
 
 @Module({
-    imports: [DomainModule, CoreModule, DaoModule, ControllerModule, DtoModule, ServiceModule, DLabsCommonModule],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [DomainModule, CoreModule,
+        DaoModule, ControllerModule,
+        DtoModule, ServiceModule,
+        DLabsCommonModule, ConfigModule.forRoot({
+            isGlobal: true
+        })]
 })
 export class AppModule {
 }
