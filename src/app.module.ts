71cf0684c5d126760ea8e3e3420b7cc08249aec4
oluwatobi Adeenkan <tadenekan@dlabs.cloud>
2020-01-26 02:02:41 +0100

    chore/auth-service update .env file with app key

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
+
 import {DomainModule} from './domain/domain.module';
 import {CoreModule} from './core/core.module';
 import {DaoModule} from './dao/dao.module';
