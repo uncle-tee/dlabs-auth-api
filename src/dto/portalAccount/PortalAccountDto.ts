19d860112d6dc5db34a6b1d884014b65754f9b18
oluwatobi Adeenkan <tadenekan@dlabs.cloud>
2020-01-27 15:52:18 +0100

    chore/auth-service add authentication service

---
+++ src/dto/portalAccount/PortalAccountDto.ts
@@ -0,0 +1,10 @@
+import {PortalAccount} from '../../domain/entity/PortalAccount';
+import {Column, OneToOne} from 'typeorm';
+import {App} from '../../domain/entity/App';
+
+export class PortalAccountDto {
+    name: string;
+    app: App;
+    @Column()
+    accountId: string;
+}
