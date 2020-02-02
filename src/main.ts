6b0b0ebb24c0e785ca20d640a63977bff4661140
oluwatobi Adeenkan <tadenekan@dlabs.cloud>
2020-02-02 18:40:37 +0100

    chore/swagger-module Added swagger!!!
    Nest Swagger Plugin not working

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,23 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {WinstonModule} from 'nest-winston';
 import {AppInterceptor} from './conf/security/interceptors/AppInterceptor';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+import {DtoModule} from './dto/dto.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {});
     app.setGlobalPrefix(`api/v${process.env.VERSION}`);
+
+    const swaggerDocumentsBuilder = new DocumentBuilder()
+        .setTitle(process.env.PROJECT_NAME)
+        .setDescription(process.env.PROJECT_DESCRIPTION)
+        .setVersion(process.env.VERSION)
+        .addTag('Dlabs Authentication API')
+        .build();
+
+    const swaggerDocument = SwaggerModule.createDocument(app, swaggerDocumentsBuilder);
+    SwaggerModule.setup('api-docs', app, swaggerDocument);
+
     await app.listen(3000);
 }
 
